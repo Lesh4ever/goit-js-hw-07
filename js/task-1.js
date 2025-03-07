55776dd6e00077a98d389a677cef032191eea1ee
Oleh Ovcharuk <leshh4ever@gmail.com>
2025-03-07 22:06:14 +0100

    1task

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const items = document.querySelectorAll("#categories .item");
+console.log(items.length);
+
+items.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const elementCount = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Element: ${elementCount}`);
+});
